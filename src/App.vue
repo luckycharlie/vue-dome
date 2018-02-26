6c6d3951b8c7a8ce28273ce8bee7d4ac113e0709
陈波 <chen.bo@datatom.com>
2018-02-26 10:57:07 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,28 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
-    <content></content>
+    <content tellChildren="i am from father"></content>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Hello from "@/components/Hello";
-import Content from "@/components/Content";
+import Hello from "./components/Hello";
+import Content from "./components/Content";
 export default {
   name: "App",
+  data() {
+    return {
+      msg:"向子组件传递信息"
+    };
+  },
   components: { Hello, Content }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
